a450036de3ea4d0117a03b51565a5073529ae7e3
Lekhelebane47 <jmotaung003@gmail.com>
2024-11-06 10:24:56 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function openLightbox() {
+    document.getElementById("lightbox").style.display = "flex";
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
+
